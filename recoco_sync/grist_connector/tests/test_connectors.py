f19df004cf7883bc99e698ace7c23b31f5411a67
Matthieu Etchegoyen <etchegom@gmail.com>
2025-02-26 16:46:04 +0100

    Fix map survey answer from question type (#5)
    * fix mp survey answer from question type
    * update tests

--- recoco_sync/grist_connector/tests/test_connectors.py
+++ recoco_sync/grist_connector/tests/test_connectors.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
-import pytest
-from grist_connector.connectors import GristConnector
+from unittest.mock import patch
 
-from .factories import GristConfigFactory
+import pytest
+from grist_connector.choices import GristColumnType
+from grist_connector.connectors import (
+    GristConnector,
+    check_table_columns_consistency,
+    grist_table_exists,
+)
+from main.utils import QuestionType
+
+from .factories import GristColumnFactory, GristConfigFactory
 
 
 @pytest.mark.django_db
@@ -31,3 +39,37 @@ class TestGristConnector:
         ) == {
             "thematiques": "Commerce rural,Participation à la vie locale,Patrimoine",
         }
+
+    @pytest.mark.parametrize(
+        "question_type,expected_grist_type",
+        [
+            (QuestionType.REGULAR, GristColumnType.TEXT),
+            (QuestionType.YES_NO, GristColumnType.BOOL),
+            (QuestionType.YES_NO_MAYBE, GristColumnType.TEXT),
+            (QuestionType.MULTIPLE, GristColumnType.CHOICE_LIST),
+        ],
+    )
+    def test_get_column_type_from_payload(self, question_type, expected_grist_type):
+        with patch(
+            "grist_connector.connectors.get_question_type",
+            return_value=question_type,
+        ):
+            assert GristConnector.get_column_type_from_payload(question_type) == expected_grist_type
+
+
+def test_grist_table_exists():
+    config = GristConfigFactory.build()
+    with patch(
+        "grist_connector.connectors.GristApiClient.table_exists", return_value=True
+    ) as mock_table_exists:
+        assert grist_table_exists(config) is True
+        mock_table_exists.assert_called_once_with(table_id=config.table_id)
+
+
+@pytest.mark.django_db
+def test_check_table_columns_consistency():
+    config = GristConfigFactory(create_columns=True, doc_id="123456789", table_id="my_table")
+    assert check_table_columns_consistency(config) is True
+
+    GristColumnFactory(grist_config=config)
+    assert check_table_columns_consistency(config) is False
--- recoco_sync/main/tests/test_tasks.py
+++ recoco_sync/main/tests/test_tasks.py
@@ -5,40 +5,39 @@ from unittest.mock import MagicMock, Mock, patch
 import pytest
 from main.choices import ObjectType, WebhookEventStatus
 from main.tasks import process_webhook_event
-from unittest_parametrize import ParametrizedTestCase, param, parametrize
 
 from .factories import WebhookEventFactory
 
 
-class ProcessWebhookEventTests(ParametrizedTestCase):
-    @parametrize(
-        "object_type, object_id, object_payload",
-        [
-            param(ObjectType.PROJECT, 999, {"project": 999}, id="project"),
-            param(ObjectType.PROJECT, 999, {"project": 999}, id="taggeditem"),
-            param(ObjectType.SURVEY_ANSWER, 888, {"project": 999}, id="survey_answer"),
-        ],
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "object_type, object_id, object_payload",
+    [
+        (ObjectType.PROJECT, 999, {"project": 999}),
+        (ObjectType.PROJECT, 999, {"project": 999}),
+        (ObjectType.SURVEY_ANSWER, 888, {"project": 999}),
+    ],
+)
+def test_task_triggered_and_event_saved(object_type, object_id, object_payload):
+    event = WebhookEventFactory(
+        object_type=object_type,
+        object_id=object_id,
+        payload={"object": object_payload},
     )
-    @pytest.mark.django_db
-    def test_task_triggered_and_event_saved(self, object_type, object_id, object_payload):
-        event = WebhookEventFactory(
-            object_type=object_type,
-            object_id=object_id,
-            payload={"object": object_payload},
-        )
 
-        fake_connector = MagicMock()
+    fake_connector = MagicMock()
 
-        with patch("main.tasks.get_connectors", Mock(return_value=[fake_connector])):
-            process_webhook_event(event_id=event.id)
+    with patch("main.tasks.get_connectors", Mock(return_value=[fake_connector])):
+        process_webhook_event(event_id=event.id)
 
-        fake_connector.update_project.assert_called_once_with(project_id=999, event=event)
+    fake_connector.update_project.assert_called_once_with(project_id=999, event=event)
 
-        event.refresh_from_db()
-        assert event.status == WebhookEventStatus.PROCESSED
+    event.refresh_from_db()
+    assert event.status == WebhookEventStatus.PROCESSED
 
-    @pytest.mark.django_db
-    def test_event_does_not_exist(self):
-        with patch("main.tasks.logger.error") as logger_mock:
-            process_webhook_event(event_id=1)
-        logger_mock.assert_called_once_with("WebhookEvent with id=1 does not exist")
+
+@pytest.mark.django_db
+def test_event_does_not_exist():
+    with patch("main.tasks.logger.error") as logger_mock:
+        process_webhook_event(event_id=1)
+    logger_mock.assert_called_once_with("WebhookEvent with id=1 does not exist")
