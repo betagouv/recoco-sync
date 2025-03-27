8f399d1b85a6c0eae164eaf98f40c22c43e019bd
Etchegoyen Matthieu <etchegom@gmail.com>
2025-03-27 14:47:22 +0100

    WIP

--- recoco_sync/les_communs_connector/clients.py
+++ recoco_sync/les_communs_connector/clients.py
@@ -14,12 +14,16 @@ class LesCommunsApiClient:
     _client: Client
 
     def __init__(self, *args, **kwargs):
+        _auth = TokenBearerAuth(
+            base_url=settings.LES_COMMUNS_API_URL,
+            username=settings.LES_COMMUNS_API_USERNAME,
+            password=settings.LES_COMMUNS_API_PASSWORD,
+        )
+        if settings.LES_COMMUNS_API_KEY:
+            _auth.access_token = settings.LES_COMMUNS_API_KEY
+
         self._client = Client(
-            auth=TokenBearerAuth(
-                base_url=settings.LES_COMMUNS_API_URL,
-                username=settings.LES_COMMUNS_API_USERNAME,
-                password=settings.LES_COMMUNS_API_PASSWORD,
-            ),
+            auth=_auth,
             base_url=settings.LES_COMMUNS_API_URL,
             event_hooks={"response": [raise_on_4xx_5xx]},
         )
@@ -29,6 +33,14 @@ class LesCommunsApiClient:
         return cls(
             # ...
         )
+
+    def list_projects(self) -> list[dict[str, Any]]:
+        response = self._client.get("/projets/")
+        return response.json()
+
+    def get_project(self, project_id: int) -> dict[str, Any]:
+        response = self._client.get(f"/projets/{project_id}/")
+        return response.json()
 
     def create_project(self, payload: dict[str, str]) -> dict[str, Any]:
         response = self._client.post("/projets/", json=payload)
