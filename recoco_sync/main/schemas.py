81029690e81a0db6ce4bc9797d1ce8e745c03f0c
Etchegoyen Matthieu <etchegom@gmail.com>
2025-04-03 17:41:59 +0200

    use pydantic to serialize recoco payloads

--- recoco_sync/main/schemas.py
+++ recoco_sync/main/schemas.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Any
 
 from ninja import Schema
+from pydantic import AliasPath, BaseModel, Field, computed_field, field_validator
 
 
 class WebhookEventSchema(Schema):
@@ -10,3 +11,49 @@ class WebhookEventSchema(Schema):
     object: dict[str, Any]
     object_type: str
     webhook_uuid: str
+
+
+class Project(BaseModel):
+    name: str
+    description: str | None
+    created_on: str = Field(alias="created")
+    updated_on: str = Field(alias="modified")
+    location: str | None
+    latitude: float | None
+    longitude: float | None
+    org_name: str | None = Field(alias="organization")
+    inactive_since: str | None
+    commune_name: str | None = Field(
+        validation_alias=AliasPath("commune", "name"),
+        alias="city",
+    )
+    commune_insee: str | None = Field(
+        validation_alias=AliasPath("commune", "insee"),
+        alias="insee",
+    )
+    commune_postal: str | None = Field(
+        validation_alias=AliasPath("commune", "postal"),
+        alias="postal_code",
+    )
+    commune_department: str | None = Field(
+        validation_alias=AliasPath("commune", "department", "name"),
+        alias="department",
+    )
+    commune_department_code: str | None = Field(
+        validation_alias=AliasPath("commune", "department", "code"),
+        alias="department_code",
+    )
+    tags: list[str] = Field(default_factory=list)
+
+    class Config:
+        populate_by_name = True
+
+    @field_validator("tags", mode="after")
+    @classmethod
+    def compute_tags(cls, value: Any) -> str:
+        return ",".join(value)
+
+    @computed_field
+    @property
+    def active(self) -> bool:
+        return self.inactive_since is None
