202499b16d4f12fe323ad23ba07f7107cf331214
alice.telescoop <alice@telescoop.fr>
2026-09-16 16:51:35 +0200

    fix: use celery to manage tasks
    revert commit that should not have been pushed

--- recoco_sync/main/triggers.py
+++ recoco_sync/main/triggers.py
@@ -8,5 +8,4 @@ from .tasks import process_webhook_event
 def on_webhook_event_commit(event: WebhookEvent) -> None:
     if event.status != WebhookEventStatus.PENDING:
         return
-    # todo do not commit removing delay
-    process_webhook_event(event.id)
+    process_webhook_event.delay(event.id)
